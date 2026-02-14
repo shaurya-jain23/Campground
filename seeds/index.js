5f83af6adc6e295cf79a348590a17f601cfe70a9
Shaurya <j.shaurya@iitg.ac.in>
2026-02-14 12:35:27 +0530

    Tweak seed generation defaults

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = array => array[Math.floor(Math.random()* array.length)]
 
 const seedDB = async ()=>{
     await Campground.deleteMany({});
-    for(let i= 0; i<50; i++){
+    for(let i= 0; i<200; i++){
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20)+10;
         const camp = await new Campground({
@@ -28,18 +28,24 @@ const seedDB = async ()=>{
             author: '682e1213881a4530a150329c',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptatem. Nesciunt, voluptates. Quos, quod. Quisquam quidem, iure, quos, voluptates, doloremque nesciunt quas voluptatem tempora quibusdam autem. Quisquam, quos. Quisquam, quos.',
             price,
-            geometry: {"type": "Point", "coordinates": [-113.1331, 47.0202]},
+            geometry: {
+                "type": "Point", 
+                "coordinates": [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
             images: [
                         {
-                            url: 'https://res.cloudinary.com/doykrr6zz/image/upload/v1748064660/YelpCamp/llqx7w1uod6gfzdqxn5h.jpg',
+                            url: 'https://res.cloudinary.com/shaurya-cloudinary/image/upload/v1748064660/YelpCamp/llqx7w1uod6gfzdqxn5h.jpg',
                             filename: 'YelpCamp/llqx7w1uod6gfzdqxn5h',
                         },
                         {
-                            url: 'https://res.cloudinary.com/doykrr6zz/image/upload/v1748064661/YelpCamp/fqqov5xjawymoyrwe8ic.jpg',
+                            url: 'https://res.cloudinary.com/shaurya-cloudinary/image/upload/v1748064661/YelpCamp/fqqov5xjawymoyrwe8ic.jpg',
                             filename: 'YelpCamp/fqqov5xjawymoyrwe8ic',
                         },
                         {
-                            url: 'https://res.cloudinary.com/doykrr6zz/image/upload/v1748331740/YelpCamp/wm0etgplg0qmxfrq3o8k.jpg',
+                            url: 'https://res.cloudinary.com/shaurya-cloudinary/image/upload/v1748331740/YelpCamp/wm0etgplg0qmxfrq3o8k.jpg',
                             filename: 'YelpCamp/wm0etgplg0qmxfrq3o8k',
                         }
                     ]
